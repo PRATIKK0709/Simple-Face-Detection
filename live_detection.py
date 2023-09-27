d4228251ec5e0f697c27b32b6a9436606088d28d
Syskey SK <139443204+PRATIKK0709@users.noreply.github.com>
2023-09-27 12:26:04 +0530

    Update live_detection.py
    slight improvement and addition at of the number of faces detected in the window.

--- live_detection.py
+++ live_detection.py
@@ -1,7 +1,6 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -11,11 +10,11 @@ while True:
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.putText(frame, f'Faces: {len(faces)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow('Real-time Face Detection', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
